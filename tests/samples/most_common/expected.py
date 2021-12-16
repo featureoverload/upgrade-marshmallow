60e9aad8f6a581b414c7d378c2b7dc84915c81c4
featureoverload <echis123@gmail.com>
2021-12-16 10:56:23 +0800

    add regression test for 4c3ecf91

--- tests/samples/most_common/expected.py
+++ tests/samples/most_common/expected.py
@@ -11,3 +11,11 @@ class FooSchema(Schema):
     def do_nothing(self, data, **_):
         """docstring keeps"""
         return data
+
+
+def normal_fun():
+    """regression test: compatible different cases without initial"""
+    pass
+
+
+normal_fun()
--- upgrade_marshmallow/testing.py
+++ upgrade_marshmallow/testing.py
@@ -1,4 +1,6 @@
 def assert_source_code_equal(left, right):
+    assert len(left) == len(right), (f"left line count {len(left)} != {len(right)} right line count")
+
     for lineno, (left_line, right_line) in enumerate(zip(left, right)):
         assert left_line == right_line, (f'line number: {lineno + 1}\n'
                                          f'left line: "{left_line}"\n'
